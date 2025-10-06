be09ca43754dbd2b6501420001cb4d1080db2237
Fabio-Vini <fbdublack17@gmail.com>
2025-10-06 13:39:38 -0300

    Iniciando projeto

---
+++ Atividade - aula - 29-09-2025 - Projeto Lista_biblioteca/listaBiblioteca/Exemplar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listaBiblioteca
+{
+    internal class Exemplar
+    {
+        private int tombo;
+        List<Emprestimo> emprestimos = new List<Emprestimo>();
+
+        public int Tombo { get { return tombo; } set {  tombo = value; } }
+
+        public Exemplar(int tombo)
+        {
+            this.tombo = tombo;
+        }
+
+        public bool emprestar()
+        {
+            return false;
+        }
+        public bool devolver()
+        {
+            return false;
+        }
+        public bool disponivel()
+        {
+            return false;
+        }
+        public int qtdeEmprestimos()
+        {
+            return 0;
+        }
+
+    }
+}
---
+++ Atividade - aula - 29-09-2025 - Projeto Lista_biblioteca/listaBiblioteca/Livro.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listaBiblioteca
+{
+    internal class Livro
+    {
+        private int isbn;
+        private string titulo;
+        private string autor;
+        private string editora;
+        List<Exemplar> exemplares = new List<Exemplar>();
+
+        public int Isbn { get {  return isbn; } set {  isbn = value; } }
+        public string Titulo { get {  return titulo; } set {  titulo = value; } }
+        public string Autor { get {  return autor; } set {  autor = value; } }
+        public string Editora { get { return editora;  } set { editora = value; } }
+
+        public Livro(int isbn, string titulo, string autor, string editora)
+        {
+            this.isbn = isbn;
+            this.titulo = titulo;
+            this.autor = autor;
+            this.editora = editora;
+        }
+
+        public void adicinarExemplar(Exemplar exemplar)
+        {
+
+        }
+
+        public int qtdeExemplares()
+        {
+            return 0;
+        }
+
+        public int qtdeDisponiveis()
+        {
+            return 0;
+        }
+
+        public int qtdeEmprestimos()
+        {
+            return 0;
+        }
+
+        public double percDisponibilidae()
+        {
+            return 0;
+        }
+    }
+}
