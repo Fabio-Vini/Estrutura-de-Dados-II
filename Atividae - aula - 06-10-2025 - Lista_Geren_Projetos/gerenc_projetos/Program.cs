71b5de61fd77a25b8a80701c1878c24238098607
Fabio-Vini <fbdublack17@gmail.com>
2025-10-13 20:52:18 -0300

    Inicio

---
+++ Atividae - aula - 06-10-2025 - Lista_Geren_Projetos/gerenc_projetos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gerenc_projetos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
